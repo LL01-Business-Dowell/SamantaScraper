8277d6d41c607d6e18592369cb27771e7f98fbe3
manishdashsharma <mdashsharma95@gmail.com>
2025-03-26 12:34:24 +0530

    base route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   const [progress, setProgress] = useState(0);
   const [taskId, setTaskId] = useState(null);
 
-  axios.defaults.baseURL = 'https://map.uxlivinglab.online';
+  axios.defaults.baseURL = 'https://map.uxlivinglab.online/api';
 
   const handleFileChange = (e) => {
     const selectedFile = e.target.files[0];
