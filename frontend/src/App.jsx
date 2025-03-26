88c2c7ceb9a007055b74221c573b7c758d5c0e75
manishdashsharma <mdashsharma95@gmail.com>
2025-03-26 12:30:08 +0530

    updated URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   const [progress, setProgress] = useState(0);
   const [taskId, setTaskId] = useState(null);
 
-  axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
+  axios.defaults.baseURL = 'https://map.uxlivinglab.online/api';
 
   const handleFileChange = (e) => {
     const selectedFile = e.target.files[0];
