4924ab71bc1ac1e326d00b7459b623180111d660
Nitesh Nair <68215942+NiteshDowell@users.noreply.github.com>
2025-05-15 11:40:43 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:8080/"
-    : "https://map.uxlivinglab.online/api"; 
+    : "https://map.uxlivinglab.online/"; 
 
 export default API_BASE_URL;
